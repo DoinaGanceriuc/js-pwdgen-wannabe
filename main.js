ff1d66d08151a5d1468774a16b6d1963c695f712
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-08 15:54:34 +0200

    complete section details
    aggiunta variabile risultato finale

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 Infine scrivi sulla pagina nomecognomecolorepreferito21
 (nome, congnome e colore preferito sono i dati inseriti dall'utente) */ 
 
-const firstName = prompt ("Ciao, qual è il tuo nome?")
-const lastName = prompt ("Qual è il tuo cognome?")
-const color = prompt ("Qual è il tuo colore preferito?")
-const result = firstName + lastName + color
+const firstName = prompt ("Ciao, qual è il tuo nome?");
+const lastName = prompt ("Qual è il tuo cognome?");
+const color = prompt ("Qual è il tuo colore preferito?");
+const result = firstName + lastName + color + 21;
 
 console.log("Il tuo nome è " + firstName);
 
 console.log("Il tuo cognome è " + lastName);
 
 console.log("Il tuo colore preferito è " + color);
+
+console.log (result);
+document.getElementById("risultato_finale").innerHTML = result;
